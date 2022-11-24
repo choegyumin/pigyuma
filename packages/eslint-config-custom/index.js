8d482059218a8b87cae502356ca9f0d731c653db
Gyumin Choi <choegyumin@gmail.com>
2022-11-24 17:17:33 +0900

    Change lint config

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,1 @@
-module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-  },
-};
+module.exports = require("./base");
---
+++ packages/eslint-config-custom/react.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ["react-app", "./base"],
+  rules: {
+    "react/jsx-key": "off",
+  },
+};
