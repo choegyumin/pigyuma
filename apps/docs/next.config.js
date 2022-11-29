4404b75af98f3e973b4b9268679ee031fa96d353
Gyumin Choi <choegyumin@gmail.com>
2022-11-30 01:05:27 +0900

    chore: supports type checking while running 'dev' script

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,6 @@
 const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import("next").NextConfig} */
@@ -7,6 +9,24 @@ const nextConfig = {
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
   experimental: {
     transpilePackages: ["ui"],
+  },
+  webpack(config, options) {
+    const { dev, isServer } = options;
+    // Do not run type checking twice:
+    if (dev && !isServer) {
+      config.plugins.push(
+        new ForkTsCheckerWebpackPlugin({
+          devServer: dev,
+          issue: {
+            exclude: [
+              { file: "**/__mocks__/**/*" },
+              { file: "**/*.test.{js,jsx,ts,tsx}" },
+            ],
+          },
+        })
+      );
+    }
+    return config;
   },
 };
 
