4c59ae0b29165987d565a882c2674ad027347c63
Gyumin Choi <choegyumin@gmail.com>
2023-02-07 18:18:12 +0900

    chore: change commit lint types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,56 +1,74 @@
+const CommitTypeEnum = {
+  feat: 'feat',
+  fix: 'fix',
+  refactor: 'refactor',
+  chore: 'chore',
+  docs: 'docs',
+  test: 'test',
+  format: 'format',
+  rename: 'rename',
+  revert: 'revert',
+  wip: 'wip',
+};
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert', 'wip']],
+    'type-enum': [2, 'always', Object.values(CommitTypeEnum)],
   },
   prompt: {
     questions: {
       type: {
         enum: {
-          feat: {
+          [CommitTypeEnum.feat]: {
             description: 'A new feature',
             title: 'Features',
             emoji: '🚀',
           },
-          fix: {
+          [CommitTypeEnum.fix]: {
             description: 'A bug fix',
             title: 'Bug Fixes',
             emoji: '🐛',
           },
-          docs: {
-            description: 'Documentation only changes',
-            title: 'Documentation',
-            emoji: '📚',
-          },
-          style: {
-            description: 'Changes that do not affect the meaning of the code (e.g.: white-space, formatting, missing semi-colons, etc)',
-            title: 'Styles',
-            emoji: '💎',
-          },
-          refactor: {
+          [CommitTypeEnum.refactor]: {
             description: 'A code change that improves performance nor improves productivity',
             title: 'Code Refactoring',
             emoji: '🏢',
           },
-          test: {
-            description: 'Adding missing tests or correcting existing tests',
-            title: 'Tests',
-            emoji: '🚨',
-          },
-          chore: {
+          [CommitTypeEnum.chore]: {
             description: "Other changes that don't modify src or test files (e.g.: Build system, External dependencies, CI)",
             title: 'Chores',
             emoji: '📦',
           },
-          revert: {
+          [CommitTypeEnum.docs]: {
+            description: 'Documentation only changes',
+            title: 'Documentation',
+            emoji: '📚',
+          },
+          [CommitTypeEnum.test]: {
+            description: 'Adding missing tests or correcting existing tests',
+            title: 'Tests',
+            emoji: '🚨',
+          },
+          [CommitTypeEnum.format]: {
+            description: 'Changes that do not affect the meaning of the code (e.g.: white-space, formatting, missing semi-colons, etc)',
+            title: 'Formatting',
+            emoji: '💎',
+          },
+          [CommitTypeEnum.rename]: {
+            description: 'Move and rename files',
+            title: 'Rename',
+            emoji: '📝',
+          },
+          [CommitTypeEnum.revert]: {
             description: 'Reverts a previous commit',
             title: 'Reverts',
             emoji: '🗑',
           },
-          wip: {
+          [CommitTypeEnum.wip]: {
             description: 'Work-In-Progress',
             title: 'WIP',
-            emoji: '🛠',
+            emoji: '👷',
           },
         },
       },
