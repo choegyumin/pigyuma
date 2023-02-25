c04a9b083589f90846f4f95d114a6432981f4c23
Gyumin Choi <choegyumin@gmail.com>
2023-02-26 05:54:48 +0900

    refactor(ui-design-tool): replace RAF to startTransition

--- packages/ui-design-tool/hooks/useUIRecordForInteraction.tsx
+++ packages/ui-design-tool/hooks/useUIRecordForInteraction.tsx
@@ -3,8 +3,9 @@ import { useItemReference, useUISubscription } from '@/components/UIDesignToolPr
 import { UIRecordKey } from '@/types/Identifier';
 import { isUIRecordKey } from '@/utils/model';
 import { useCloneDeepState } from '@pigyuma/react-utils';
-import { useEffect } from 'react';
+import { startTransition, useEffect } from 'react';
 
+/** @todo 렌더링이 늦을 경우: startTransition을 RAF로 변경 (3ba4789) */
 export default function useUIRecordForInteraction(recordKey: UIRecordKey | undefined) {
   const { subscribeItem } = useUISubscription();
   const getRecord = useItemReference();
@@ -14,33 +15,25 @@ export default function useUIRecordForInteraction(recordKey: UIRecordKey | undef
   const [record, setRecord] = useCloneDeepState<UIRecord | undefined>(undefined);
 
   useEffect(() => {
-    const requestId = window.requestAnimationFrame(() => {
+    startTransition(() => {
       setRecord(isUIRecordKey(recordKey) ? getRecord(recordKey) : undefined);
     });
-    return () => {
-      window.cancelAnimationFrame(requestId);
-    };
   }, [recordKey, setRecord, getRecord]);
 
   useEffect(() => {
     if (!isUIRecordKey(recordKey)) {
       return;
     }
-    let requestId = -1;
     const callback = (newRecord: UIRecord | undefined) => {
-      // 이미 렌더링 된 UIRecord 엘리먼트에 접근해야 하므로,
-      // UIRecord 컴포넌트의 재조정이 일어난 직후 dipatch를 실행해 늦은 재조정을 유발함
-      // 렌더링 시점에 오차가 발생하지만 free transform 시 handle을 숨겨 눈에 띄지 않음
-      window.cancelAnimationFrame(requestId);
-      requestId = window.requestAnimationFrame(() => {
+      // 이미 렌더링이 끝난 엘리먼트에 접근하기 위해 상태 변경을 지연시킴
+      // (callback 실행 시점은 다른 상태 변경과 동일하지만, queue는 따로 관리되는 것으로 추측됨)
+      // (startTransition의 용도와 해결하려는 문제가 서로 상충해 오히려 렌더링이 늦어질 수 있음)
+      startTransition(() => {
         setRecord(newRecord);
       });
     };
     const unsubscribe = subscribeItem(recordKey, callback);
-    return () => {
-      unsubscribe();
-      window.cancelAnimationFrame(requestId);
-    };
+    return unsubscribe;
   }, [recordKey, setRecord, subscribeItem]);
 
   return record;
